f3eda027199c17866ba2a11a8050c827df34cec3
Ryan Carbotte <ryancarbotte@gmail.com>
2023-05-14 19:31:33 -0500

    Implement window message procedure handling

--- src/main.cpp
+++ src/main.cpp
@@ -591,3 +591,66 @@ void SetFullscreen(bool fullscreen)
 		}
 	}
 }
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	if (g_IsInitialized)
+	{
+		switch (message)
+		{
+		case WM_PAINT:
+			Update();
+			Render();
+			break;
+		case WM_SYSKEYDOWN:
+		case WM_KEYDOWN:
+		{
+			bool alt = (::GetAsyncKeyState(VK_MENU) & 0x8000) != 0;
+
+			switch (wParam)
+			{
+			case 'V':
+				g_VSync = !g_VSync;
+				break;
+			case VK_ESCAPE:
+				::PostQuitMessage(0);
+				break;
+			case VK_RETURN:
+				if (alt)
+				{
+			case VK_F11:
+				SetFullscreen(!g_Fullscreen);
+				}
+				break;
+			}
+		}
+		break;
+		// The default window procedure will play a system notification sound when pressing the Alt+Enter
+		// keyboard combination if this message is not handled.
+		case WM_SYSCHAR:
+			break;
+		case WM_SIZE:
+		{
+			RECT clientRect = {};
+			::GetClientRect(g_hWnd, &clientRect);
+
+			int width = clientRect.right - clientRect.left;
+			int height = clientRect.bottom - clientRect.top;
+
+			Resize(width, height);
+		}
+		break;
+		case WM_DESTROY:
+			::PostQuitMessage(0);
+			break;
+		default:
+			return ::DefWindowProcW(hWnd, message, wParam, lParam);
+		}
+	}
+	else
+	{
+		return ::DefWindowProcW(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
